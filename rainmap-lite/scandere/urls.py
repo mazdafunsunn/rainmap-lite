7679677dba7c59bb69719e13f7c6fdcc41b6b017
MazdaFunSun <tm1717@gmail.com>
2018-08-08 11:34:16 +0300

    django 2.1 compatability

--- rainmap-lite/scandere/urls.py
+++ rainmap-lite/scandere/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^console/clear_logs/?$', adm_views.clear_logs, name='clear-logs'),
     url(r'^console/', admin.site.urls),
-    url(r'^login/$', views_django.login, name='login')
+    url(r'^login/$', views_django.LoginView.as_view(template_name='registration/login.html'), name='login')
 ]
